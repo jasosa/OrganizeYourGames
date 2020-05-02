6d04f15babd260e378976130e8befc15e73e48ea
Juan Antonio Sosa <juanan.sosa@gmail.com>
2020-05-02 18:13:53 +0200

    172634261 - Game page is shown (WIP) when game is selected in home page

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrganizeYourGames.Models;
+
+namespace OrganizeYourGames.Controllers
+{
+    [Route("games")]
+    public class GamesController : Controller
+    {
+        private GamesDataContext data;
+
+        public GamesController(GamesDataContext data)
+        {
+            this.data = data;
+        }
+
+
+        [Route("{name}")]
+        public IActionResult Inserts(string name)
+        {
+            var game = data.Games.Where(g => g.Name == name).SingleOrDefault();
+            return View(game);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,16 +12,16 @@ namespace OrganizeYourGames.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;        
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger;            
         }
 
         public IActionResult Index()
         {            
             return View();
-        }
+        }       
     }
 }
