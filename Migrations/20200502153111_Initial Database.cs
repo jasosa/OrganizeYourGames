b81422e8ed665b3df4474fec7bd0b2cc01d84821
Juan Antonio Sosa <juanan.sosa@gmail.com>
2020-05-02 17:37:03 +0200

    172403733 - Added migrations and test data

---
+++ Migrations/20200502153111_Initial Database.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganizeYourGames.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    Publisher = table.Column<string>(nullable: true),
+                    Version = table.Column<string>(nullable: true),
+                    PublishedIn = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    BBGLink = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true),
+                    NumberOfOrganizers = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inserts",
+                columns: table => new
+                {
+                    Key = table.Column<string>(nullable: false),
+                    Game = table.Column<string>(nullable: true),
+                    Store = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    LinkToBuy = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inserts", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Inserts");
+        }
+    }
+}
---
+++ TestData/dbo.Inserts.data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Inserts] ([Key], [Game], [Store], [Price], [LinkToBuy], [Image]) VALUES (N'51st_broken_token', N'51st State', N'The Broken Token', 36.99, N'https://thebrokentoken.com/51st-state-organizer', N'51stState_BrokenToken')
+INSERT INTO [dbo].[Inserts] ([Key], [Game], [Store], [Price], [LinkToBuy], [Image]) VALUES (N'andor_game_doctors', N'Legends of Andor', N'The Game Doctors', 21.17, N'https://sales.game-drs.de/en/for-hybrid-games/38-leva-0751385593648.html', N'Andor_GameDoctors')
